8306e7ea2c1aaff679775a14a403c0ea942ace2d
ianmello <ianomello@outlook.com>
2023-05-05 09:08:18 -0300

    TAREFA_AULA_09

--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,35 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_sum():
+    value1 = 3
+    value2 = 6
+
+    output = methods.soma(value1, value2)
+
+    assert output == 9
+
+def test_mult():
+    value1 = 7
+    value2 = 2
+
+    output = methods.multiplicacao(value1, value2)
+
+    assert output == 14
+
+def test_div():
+    value1 = 20
+    value2 = 2
+
+    output = methods.divisao(value1, value2)
+
+    assert output == 10
+
+def test_sub():
+    value1 = 9
+    value2 = 3
+
+    output = methods.subtracao(value1, value2)
+
+    assert output == 6
